a34d4e5e91e9886a0d45595b987fa10c96611d21
Delonel123 <gatilov.anton@mail.ru>
2021-08-16 18:25:06 +0300

    deleted

--- src/utils/validators/validators.js
+++
@@ -1,9 +0,0 @@
-export const requried = (value) =>{
-    if(value) return undefined
-    return 'Field is required';
-}
-
-export const maxLengthCreator = (maxLength) => (value) =>{
-    if(value.length > maxLength) return `Max Length is ${maxLength} symbols`
-    return undefined;
-}
